eca57f056957308217ddc1abe7b510a5a0c95831
Khoa <khoanguyenandhisjourney@gmail.com>
2026-02-14 23:16:47 -0500

    I need help, something must've gone wrong with my installatin for daisy and tailwind, i couldnt get daisy theme to work

---
+++ 2026-12-2_project/src/App.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore
+import "./App.css";
+
+function App() {
+  return (
+    <div data-theme="cyberpunk" className="min-h-screen w-full flex items-center justify-center bg-base-100">
+      <p className="text-5xl font-extrabold text-primary drop-shadow-lg bg-base-200 bg-opacity-60 px-8 py-4 rounded-xl">
+        Hello Cyberpunk
+      </p>
+    </div>
+  )
+}
+
+export default App
